a94817a180fbfae56ce18d209858a46c8acb76d3
Niklas Bauer <nikbau2000@gmail.com>
2022-12-04 11:32:15 +0100

    Day 4: Better RegEx

--- solutions/day04.py
+++ solutions/day04.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
+import re
 
-from utils import advent, helpers
+from utils import advent
 
 advent.setup(2022, 4)
 fd = advent.get_input()
+pat = re.compile(r"(\d+)-(\d+),(\d+)-(\d+)\n")
 
-input = list(zip(*[iter(helpers.get_ints(fd, True, r"\d+"))] * 4))
+input = [list(map(int, re.match(pat, l).groups())) for l in fd.readlines()]
 
 sum1 = sum(x >= a and y <= b or a >= x and b <= y for a, b, x, y in input)
 sum2 = sum(b >= x and a <= y for a, b, x, y in input)
