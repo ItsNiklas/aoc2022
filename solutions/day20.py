53b23c57e4b679bf2ca8fd6ea1b7e89a479eb6bb
Niklas Bauer <nikbau2000@gmail.com>
2022-12-20 15:16:45 +0100

    Day 20

--- solutions/day20.py
+++ solutions/day20.py
@@ -1,34 +1,35 @@
 #!/usr/bin/env python3
 
-import io
-import sys
 from random import getrandbits
 
 from utils import advent
 
 advent.setup(2022, 20)
-fd = advent.get_input() if not 'debug' in sys.argv else io.StringIO("""\
-1
-2
--3
-3
--2
-0
-4
-""")
+fd = advent.get_input()
 
-# Making entries unique so that I can call .index() by appending a random hash.
-file: list[str] = list(map(lambda s: s.strip() + str(hex(getrandbits(32))), fd))
-N = len(file)
 
-for di in file.copy():
-    i = file.index(di)
-    offset = i + int(di.split('0x')[0])
+def mixing(times, DECRYPTION_KEY) -> int:
+    fd.seek(0)
 
-    file.pop(i)
-    file.insert(offset % (N - 1), di)
+    # Making entries unique by appending a random hash so that I can call .index().
+    file: list[str] = list(
+        map(lambda s: str(int(s) * DECRYPTION_KEY) + str(hex(getrandbits(32))), fd)
+    )
+    N = len(file)
+    pre_mixed = file.copy()
 
-    if 'debug' in sys.argv: print(file)
+    for _ in range(times):
+        for di in pre_mixed:
+            i = file.index(di)
+            offset = i + int(di.split("0x")[0])
 
-z_i = list(x.startswith('00x') for x in file).index(True)
-advent.print_answer(1, sum(int(file[(z_i + 1000 * i) % N].split('0x')[0]) for i in range(1, 4)))
+            file.pop(i)
+            file.insert(offset % (N - 1), di)
+
+    # Find index of 0 and sum 1000, 2000 & 3000
+    z_i: int = list(x.startswith("00x") for x in file).index(True)
+    return sum(int(file[(z_i + 1000 * i) % N].split("0x")[0]) for i in range(1, 4))
+
+
+advent.print_answer(1, mixing(1, 1))
+advent.print_answer(2, mixing(10, 811589153))
